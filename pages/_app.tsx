8a3e7f04089f989fb7dae8bc2a80cdb5d2552131
Niranjith Perumal <niranjith.perumal@inovaitsys.com>
2022-04-15 09:42:03 +0530

    refactor(home): update home to pull hero data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
+
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
+import { SWRConfig } from "swr";
+
 import theme from "../utils/theme";
 import emotionCache from "../utils/emotion-cache";
 
@@ -21,9 +24,14 @@ export default function MyApp(props: MyAppProps) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
       </ThemeProvider>
     </CacheProvider>
   );
---
+++ pages/heros/[id].tsx
@@ -0,0 +1,52 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+
+import { Hero } from "../../interfaces";
+import { sampleUserData } from "../../utils/sample-data";
+import ListDetail from "../../components/ListDetail";
+
+type Props = {
+  item?: Hero;
+  errors?: string;
+};
+
+const StaticPropsDetail = ({ item, errors }: Props) => {
+  if (errors) {
+    return (
+      <>
+        <p>
+          <span style={{ color: "red" }}>Error:</span> {errors}
+        </p>
+      </>
+    );
+  }
+
+  return <>{item && <ListDetail item={item} />}</>;
+};
+
+export default StaticPropsDetail;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Get the paths we want to pre-render based on users
+  const paths = sampleUserData.map((user) => ({
+    params: { id: user.id.toString() },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+};
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    const id = params?.id;
+    const item = sampleUserData.find((data) => data.id === Number(id));
+    // By returning { props: item }, the StaticPropsDetail component
+    // will receive `item` as a prop at build time
+    return { props: { item } };
+  } catch (err: any) {
+    return { props: { errors: err.message } };
+  }
+};
