9a93be8996c7cc09f719fdfd550f82f5b65f2a35
Niranjith Perumal <niranjith.perumal@inovaitsys.com>
2022-04-15 16:57:49 +0530

    feat(cards): updated cards ui

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -3,26 +3,41 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Hero } from "../types";
+import styles from "../styles/Card.module.css";
 
 type Props = {
   data: Hero;
 };
 
 const ListItem = ({ data }: Props) => (
-  <>
-    <Image src={data.images.sm} alt={data.slug} width={160} height={240} />
-    <h2>
-      <Link href="/heros/[id]" as={`/heros/${data.id}`}>
-        <a>{data.name}</a>
-      </Link>
-    </h2>
-    <ul>
-      <li>Fullname: {data.biography.fullName ?? "N/A"}</li>
-      <li>Race: {data.appearance.race ?? "N/A"}</li>
-      <li>Alignment: {data.biography.alignment ?? "N/A"}</li>
-      <li>Publisher: {data.biography.publisher ?? "N/A"}</li>
-    </ul>
-  </>
+  <div className={styles.card}>
+    <Image
+      src={data.images.sm}
+      alt={data.slug}
+      width={160}
+      height={240}
+      className={styles.cardImage}
+    />
+
+    <div className={` ${styles.cardContent} ${styles["card-" + data.biography.alignment]}`}>
+      <h2 className={styles.cardTitle}>
+        <Link href="/heros/[id]" as={`/heros/${data.id}`}>
+          <a>{data.name}</a>
+        </Link>
+      </h2>
+      <div className={styles.cardData}>
+        <ul>
+          <li>Fullname: {data.biography.fullName ?? "N/A"}</li>
+          <li>Race: {data.appearance.race ?? "N/A"}</li>
+          <li>Alignment: {data.biography.alignment ?? "N/A"}</li>
+          <li>Publisher: {data.biography.publisher ?? "N/A"}</li>
+        </ul>
+      </div>
+    </div>
+    <div className={styles.cardButton}>
+      <button>+</button>
+    </div>
+  </div>
 );
 
 export default ListItem;
